e5cce95efd85db981e7077d41103283624ee2f39
vikumkanishka <vikumkanishka14@gmail.com>
2026-01-14 02:28:22 +0530

    Add initial project structure XML configurations, and RoomInfo functionality

---
+++ src/main/java/Starter.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/room_info.fxml"))));
+        stage.show();
+    }
+}
---
+++ src/main/java/controller/RoomInfoFormController.java
@@ -0,0 +1,98 @@
+package controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.dto.RoomInfoDTO;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class RoomInfoFormController implements Initializable {
+
+    @FXML
+    private TableColumn<?, ?> colAvailability;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colFloor;
+
+    @FXML
+    private TableColumn<?, ?> colMaxGuests;
+
+    @FXML
+    private TableColumn<?, ?> colPricePerNight;
+
+    @FXML
+    private TableColumn<?, ?> colRoomId;
+
+    @FXML
+    private TableColumn<?, ?> colRoomType;
+
+    @FXML
+    private TableView<RoomInfoDTO> tblRoomInfo;
+
+    ObservableList <RoomInfoDTO> observableList = FXCollections.observableArrayList();
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+        loadDetails();
+
+    }
+
+    private void loadDetails(){
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_reservation_system","root","200004602360");
+
+            ResultSet resultSet = connection.prepareStatement("SELECT * FROM rooms").executeQuery();
+
+            observableList.clear();
+
+            while (resultSet.next()){
+                String roomId = resultSet.getString("room_Id");
+                String roomType = resultSet.getString("type");
+                String description = resultSet.getString("description");
+                double pricePerNight = resultSet.getDouble("price_Per_Night");
+                int maxGuests = resultSet.getInt("max_Guests");
+                int floor = resultSet.getInt("floor");
+                boolean availability = resultSet.getBoolean("availability");
+
+                RoomInfoDTO roomInfoDTO = new RoomInfoDTO(roomId,roomType,description,pricePerNight,maxGuests,floor,availability);
+
+                observableList.add(roomInfoDTO);
+            }
+
+            colRoomId.setCellValueFactory(new PropertyValueFactory<>("roomId"));
+            colRoomType.setCellValueFactory(new PropertyValueFactory<>("roomType"));
+            colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+            colPricePerNight.setCellValueFactory(new PropertyValueFactory<>("pricePerNight"));
+            colMaxGuests.setCellValueFactory(new PropertyValueFactory<>("maxGuests"));
+            colFloor.setCellValueFactory(new PropertyValueFactory<>("floor"));
+            colAvailability.setCellValueFactory(new PropertyValueFactory<>("availability"));
+
+            tblRoomInfo.setItems(observableList);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadDetails();
+    }
+}
