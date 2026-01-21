40ecd956129171f05376de806f1569de8bcb00b9
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-01-21 11:08:34 +0530

    Add Dashboard.fxml and DashboardController for main application interface

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class DashboardController {
+
+    Stage stage = new Stage();
+    
+    public void btnCustomerInfoOnAction(ActionEvent actionEvent) {
+        Stage oldStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_Info.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        oldStage.close();
+        stage.show();
+    }
+
+    public void btnRoomInfoOnAction(ActionEvent actionEvent) {
+    }
+}
